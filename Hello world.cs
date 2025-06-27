02f2cbdb1a6d3c9c87b021be764cdc72b5535248
POP8891 <popapaipai17@gmail.com>
2025-06-27 17:29:30 +0300

    Add files via upload

---
+++ Hello world.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class SCRIPT: MonoBehaviour
+ 
+{
+    void Start()
+    {
+        print("Hello World");
+    }
+    void Update ()
+    {
+
+    }
+}
